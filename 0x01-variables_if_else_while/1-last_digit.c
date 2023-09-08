30f9004089d93942cbeb30534b75250747a090d4
root <efaquaysonalex124@gmail.com>
2023-09-08 11:39:56 -0700

    added 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - Entry point
- * Description: This is the main program that  will assign a random number to the variable 'n'.
+ * Description: This is the main program that will assign a random number to the variable 'n'.
  * Return: Always 0 (Success)
  */
 int main(void)
