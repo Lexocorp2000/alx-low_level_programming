b91a3371c4ba2224f86a657706e4baab73a527b3
root <efaquaysonalex124@gmail.com>
2023-09-12 17:47:09 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,66 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
-
-/* Protype for the _putchar function */
-int _putchar(char c);
-
-/* Prototype for the print_alphabet_x10 function */
-void print_alphabet_x10(void);
-
-#endif /* MAIN_H */
-
 #include "main.h"
 
 /**
- * _putchar - Custom putchar function
- *
- * @c: The character to print
- *
- * Return: On success, returns the number of characters written (1)
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_alphabet - Prints the alphabet in lowercase.
- */
-void print_alphabet(void)
-{
-	char letter;
-
-	for  (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
-}
-
-/**
- * print_alphabet_x10 - Prints the alphabets in lowercase 10 times.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(j);
 	}
-}
-
-#include "main.h"
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet_x10();
-
-	return (0);
+	_putchar('\n');
+	}
 }
