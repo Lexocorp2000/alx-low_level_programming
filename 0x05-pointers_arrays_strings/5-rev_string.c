e6f373388ea2bba95641cf80a3a197399cc1723f
root <efaquaysonalex124@gmail.com>
2023-09-18 15:14:17 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	length--;
+
+	while (start < length - 1)
+	{
+		temp = s[start];
+		s[start] = s[length - 1];
+		s[length - 1] = temp;
+	
+	start++;
+	length--;
+	}
+}
+
