e822fcf3bd0db7c192bcddd5c8cf53b802f40fc9
root <efaquaysonalex124@gmail.com>
2023-10-04 15:00:47 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,46 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the integer converted from the string
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: 0 on success, 1 on error
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-	int i, d, n, len, f, digit;
+	int num1, num2, result;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-	len++;
-
-	while (i < len && f == 0)
+	if (argc != 3)
 	{
-	if (s[i] == '-')
-	++d;
+	printf("Error\n");
+	return (1);
+	}
 
-	if (s[i] >= '0' && s[i] <= '9')
-	{
-	digit = s[i] - '0';
-	if (d % 2)
-	digit = -digit;
-	n = n * 10 + digit;
-	f = 1;
-	if (s[i + 1] < '0' || s[i + 1] > '9')
-	break;
-	f = 0;
-	}
-	i++;
-	}
-	if (f == 0)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
 	return (0);
-
-	return n;
 }
